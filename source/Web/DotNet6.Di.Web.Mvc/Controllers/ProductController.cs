6cf8e475faf9cd4370178b2d61afda703a430f88
Dave <davidgrace00@gmail.com>
2022-01-13 18:28:00 +0000

    Added ProductService into ProductController

--- source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
+++ source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
@@ -1,17 +1,30 @@
-﻿using DotNet6.Di.Libraries.Services.Product.Models;
+﻿using DotNet6.Di.Libraries.Services.Product;
+using DotNet6.Di.Libraries.Services.Product.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet6.Di.Web.Mvc.Controllers
 {
     [Route("product")]
     public class ProductController : Controller
-    {
-       
+    {   
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
 
         [HttpGet("{sku}")]
         public IActionResult Get(string sku)
         {
-            return View();
+            var product = _productService.Get(sku);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
     }
 }
