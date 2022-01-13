fca7d8de1e4179b115edc08ebfaf61b8556633ef
Dave <davidgrace00@gmail.com>
2022-01-13 13:25:21 +0000

    Add services to the IoC container.

---
+++ source/Web/DotNet6.Di.Web.Mvc/Startup.cs
@@ -0,0 +1,55 @@
+﻿using DotNet6.Di.Libraries.Services.Product;
+using DotNet6.Di.Libraries.Services.ShoppingCart;
+using DotNet6.Di.Libraries.Services.Storage;
+
+namespace DotNet6.Di.Web.Mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add dependency injection.
+            services.AddSingleton<IStorageService, StorageService>();
+            services.AddScoped<IShoppingCartService, ShoppingCartService>();
+            services.AddTransient<IProductService, ProductService>();
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
