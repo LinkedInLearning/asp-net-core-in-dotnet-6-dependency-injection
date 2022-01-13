3bad77d57fba5f4b664704eefa4ea85a51890096
Dave <davidgrace00@gmail.com>
2022-01-13 18:23:14 +0000

    Added images

---
+++ source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using DotNet6.Di.Libraries.Services.Product.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet6.Di.Web.Mvc.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+       
+
+        [HttpGet("{sku}")]
+        public IActionResult Get(string sku)
+        {
+
+        }
+    }
+}
