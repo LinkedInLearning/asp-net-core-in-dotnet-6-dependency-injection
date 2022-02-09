00fa2f1fdd4ff24432efe4ac5e12193ffbd2671a
Dave <davidgrace00@gmail.com>
2022-02-09 19:07:34 +0000

    Changes to Shopping Cart Service
    (cherry picked from commit 0fb094dcc188f529ce27f049ef3134ce206a4047)

--- source/Libraries/DotNet6.Di.Libraries.Services/ShoppingCart/ShoppingCartService.cs
+++ source/Libraries/DotNet6.Di.Libraries.Services/ShoppingCart/ShoppingCartService.cs
@@ -1,4 +1,5 @@
-﻿using DotNet6.Di.Libraries.Services.ShoppingCart.Models;
+﻿using DotNet6.Di.Libraries.Services.Product.Models;
+using DotNet6.Di.Libraries.Services.ShoppingCart.Models;
 using DotNet6.Di.Libraries.Services.Storage;
 
 namespace DotNet6.Di.Libraries.Services.ShoppingCart
@@ -28,17 +29,7 @@ namespace DotNet6.Di.Libraries.Services.ShoppingCart
         }
 
         /// <summary>
-        /// Sets the unique id of the shopping cart and adds it to the storage.
-        /// </summary>
-        /// <param name="id">Unique id of the shopping cart.</param>
-        public void SetId(Guid id)
-        {
-            Id = id;
-            _storageService.AddShoppingCart(id);
-        }
-
-        /// <summary>
-        /// Gets the shopping cart model.
+        /// Gets the current shopping cart.
         /// </summary>
         /// <returns>The shopping cart as a <see cref="ShoppingCartModel"/> type.</returns>
         /// <exception cref="Exception">Returns an exception if the shopping cart cannot be found.</exception>
@@ -53,6 +44,25 @@ namespace DotNet6.Di.Libraries.Services.ShoppingCart
         }
 
         /// <summary>
+        /// Adds a product to the current shopping cart.
+        /// </summary>
+        /// <param name="product">An instance of the product</param>
+        /// <param name="quantity">The quantity they wish to add.</param>
+        public void AddProduct(ProductModel product, int quantity)
+        {
+            Get().Items.Add(new ShoppingCartItemModel(product, quantity));
+        }
+
+        /// <summary>
+        /// Gets the number of items added to the current shopping cart.
+        /// </summary>
+        /// <returns>The total number of items.</returns>
+        public int Count()
+        {
+            return Get().Items.Count();
+        }
+
+        /// <summary>
         /// Has a product been added to the shopping cart?
         /// </summary>
         /// <param name="sku">The unique identifier of the product.</param>
@@ -62,6 +72,16 @@ namespace DotNet6.Di.Libraries.Services.ShoppingCart
             var shoppingCart = Get();
 
             return shoppingCart.Items.Any(i => i.Product.Sku == sku);
+        }
+
+        /// <summary>
+        /// Sets the unique id of the shopping cart and adds it to the storage.
+        /// </summary>
+        /// <param name="id">Unique id of the shopping cart.</param>
+        public void SetId(Guid id)
+        {
+            Id = id;
+            _storageService.AddShoppingCart(id);
         }
     }
 }
