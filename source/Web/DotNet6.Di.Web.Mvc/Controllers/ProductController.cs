302bd9f35227a513c477a454472c44655b922f9a
David Grace <davidgrace00@gmail.com>
2022-02-12 10:30:34 +0000

    Add parameter

--- source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
+++ source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
@@ -1,12 +1,16 @@
 ﻿using DotNet6.Di.Libraries.Services.Product.Models;
 using Microsoft.AspNetCore.Mvc;
+using DotNet6.Di.Libraries.Services.Product;
 
 namespace DotNet6.Di.Web.Mvc.Controllers
 {
     [Route("product")]
     public class ProductController : Controller
     {
-       
+        public ProductController()
+        {
+
+        }
 
         [HttpGet("{sku}")]
         public IActionResult Get(string sku)
