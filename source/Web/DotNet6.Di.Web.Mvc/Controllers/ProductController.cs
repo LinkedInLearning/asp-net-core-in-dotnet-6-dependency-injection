be160c46ffd258ea8b970c0d9b27f528b433833d
David Grace <davidgrace00@gmail.com>
2022-02-12 10:33:01 +0000

    Remove constructor

--- source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
+++ source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
@@ -7,11 +7,6 @@ namespace DotNet6.Di.Web.Mvc.Controllers
     [Route("product")]
     public class ProductController : Controller
     {
-        public ProductController()
-        {
-
-        }
-
         [HttpGet("{sku}")]
         public IActionResult Get(string sku)
         {
