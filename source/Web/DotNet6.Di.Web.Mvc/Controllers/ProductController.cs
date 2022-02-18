ea63ffafcc0c60ed4e0f36da090ad8704f30a5ef
David Grace <davidgrace00@gmail.com>
2022-02-18 20:11:06 +0000

    Merge changes in from 03_02

--- source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
+++ source/Web/DotNet6.Di.Web.Mvc/Controllers/ProductController.cs
@@ -1,17 +1,20 @@
 ﻿using DotNet6.Di.Libraries.Services.Product;
 using DotNet6.Di.Libraries.Services.Product.Models;
+using DotNet6.Di.Libraries.Services.ShoppingCart;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet6.Di.Web.Mvc.Controllers
 {
     [Route("product")]
     public class ProductController : BaseController
-    {   
+    {
         private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
 
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, IShoppingCartService shoppingCartService)
         {
             _productService = productService;
+            _shoppingCartService = shoppingCartService;
         }
 
         [HttpGet("{sku}")]
@@ -26,5 +29,21 @@ namespace DotNet6.Di.Web.Mvc.Controllers
 
             return View(product);
         }
+
+        [HttpPost("{sku}")]
+        public IActionResult AddToShoppingCart(string sku)
+        {
+            var product = _productService.Get(sku);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _shoppingCartService.AddProduct(product, 1);
+
+            return RedirectToAction("Get", new { sku });
+        }
+
     }
 }
